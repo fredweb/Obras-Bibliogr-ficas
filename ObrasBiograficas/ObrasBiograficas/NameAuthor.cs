a58c3e8597afd554e1d8ff6fcb1f01856ba9cccf
George Santos <fredweb30@hotmail.com>
2019-06-16 15:09:05 -0300

    ç

---
+++ ObrasBiograficas/ObrasBiograficas/NameAuthor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace ObrasBiograficas
+{
+    public class NameAuthor
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        private readonly string[] cases = new[]
+        {
+            "da", "de", "do", "das", "dos"
+        };
+        public NameAuthor()
+        {
+
+        }
+
+        public NameAuthor(string fullName)
+        {
+            var nameParts = fullName.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (nameParts == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < nameParts.Length; i++)
+            {
+                if (i == 0)
+                    FirstName = nameParts[i];
+                else if (i == 1)
+                {
+                    if (cases.Contains(nameParts[i].ToLower()))
+                        MiddleName = nameParts[i].ToLower();
+                    else
+                        LastName = LastName + " " + nameParts[i].ToUpper();
+                }
+                else
+                {
+                    var name = cases.Contains(nameParts[i].ToLower()) ? nameParts[i].ToLower() : nameParts[i].ToUpper();
+                    LastName = LastName + " " + name;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstCharToUpper(FirstName)} {MiddleName}".TrimEnd();
+        }
+
+        public static string FirstCharToUpper(string FisrtName)
+        {
+            switch (FisrtName)
+            {
+                case null: throw new ArgumentNullException(nameof(FisrtName));
+                case "": throw new ArgumentException($"{nameof(FisrtName)} cannot be empty", nameof(FisrtName));
+                default: return FisrtName.First().ToString().ToUpper() + FisrtName.Substring(1);
+            }
+        }
+    }
+}
---
+++ ObrasBiograficas/ObrasBiograficas/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObrasBiograficas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberNames = default(int);
+            var names = new List<NameAuthor>();
+            string texto = "Por favor, digite um número de Nomes: ";
+            string mensagemDeErro = "Valor digitado inválido!";
+            int valor = GetInteger(texto, mensagemDeErro);
+
+            for (int i = 0; i < valor; i++)
+            {
+                Console.WriteLine("Digite o nome do autor.");
+                string input = Console.ReadLine();
+                names.Add(new NameAuthor(input));
+            }
+
+            foreach (var autor in names)
+            {
+                Console.WriteLine(autor.ToString());
+            }
+
+        }
+
+        public static int GetInteger(string texto, string mensagemDeErro)
+        {
+            Console.WriteLine(texto);
+            string input = Console.ReadLine();
+            int valor = 0;
+
+            if (int.TryParse(input, out valor) && valor > 0)
+            {
+                return valor;
+            }
+            else
+            {
+                Console.WriteLine(mensagemDeErro);
+                return GetInteger(texto, mensagemDeErro);
+            }
+        }
+    }
+}
